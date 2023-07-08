706b704dfe8c822fd45edc4582e1693dbafce627
marsmallowz <alsandymaulana15@email.com>
2023-07-09 00:02:16 +0700

    Add carousel

---
+++ components/Carousel.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = ["carousel/image-1.svg", "carousel/image-2.svg"];
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleDotClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(handleNext, 5000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="relative p-5">
+      <Image
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex + 1}`}
+        width={600}
+        height={400}
+        className="mx-auto"
+      />
+      <div className="flex justify-center mt-4">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className={`h-3 w-3 mx-1 rounded-full ${
+              index === currentIndex ? "bg-gray-800" : "bg-gray-400"
+            }`}
+            onClick={() => handleDotClick(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
