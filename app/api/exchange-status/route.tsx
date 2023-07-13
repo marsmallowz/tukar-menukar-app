5a95f9cd7a901fccb0e97ba741bffb9e767d2997
marsmallowz <alsandymaulana15@email.com>
2023-07-13 10:07:07 +0700

    delete unused api

--- app/api/exchange-status/route.tsx
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-export function GET(request: NextRequest) {
-  console.log("cronjob");
-
-  return NextResponse.json("success");
-}
