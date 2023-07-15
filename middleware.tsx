8c3dfee228c6da7b27a782054637ac1a9868b33e
marsmallowz <alsandymaulana15@email.com>
2023-07-16 00:14:41 +0700

    redirect to dashboard if  auth & access some page

--- middleware.tsx
+++ middleware.tsx
@@ -1,33 +1,70 @@
+import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(req) {
-    // console.log("withauth jalan");
-    // console.log(req.nextauth);
-    // if (
-    //   (req.nextUrl.pathname === "/register" ||
-    //     req.nextUrl.pathname === "/login") &&
-    //   req.nextauth.token !== null
-    // ) {
-    //   return NextResponse.redirect(new URL("/", req.url));
-    // }
-    // if (req.nextUrl.pathname === "/settings" && req.nextauth.token === null) {
-    //   return NextResponse.redirect(new URL("/login", req.url));
-    // }
-  },
-  {
-    callbacks: {
-      authorized: (params) => {
-        let { token } = params;
-        return !!token;
-      },
-    },
-    pages: {
-      signIn: "/login",
-    },
+export default async function middleware(
+  req: NextRequest,
+  event: NextFetchEvent
+) {
+  const token = await getToken({ req });
+  const isAuthenticated = !!token;
+
+  if (
+    req.nextUrl.pathname.startsWith("/login") ||
+    req.nextUrl.pathname.startsWith("/register") ||
+    req.nextUrl.pathname.startsWith("/verify")
+  ) {
+    if (isAuthenticated) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
   }
-);
+  const authMiddleware = await withAuth({
+    pages: {
+      signIn: `/login`,
+    },
+  });
+
+  // @ts-expect-error
+  return authMiddleware(req, event);
+}
+
+// tidak bisa digunakan karena terjadi bug jika menggunakan matcher "/login"
+// export default withAuth(
+//   async function middleware(req) {
+//     const token = await getToken({ req });
+//     console.log(token);
+//     const isAuthenticated = !!token;
+
+//     if (
+//       req.nextUrl.pathname.startsWith("/login") ||
+//       req.nextUrl.pathname.startsWith("/register") ||
+//       req.nextUrl.pathname.startsWith("/verify")
+//     ) {
+//       if (isAuthenticated) {
+//         return NextResponse.redirect(new URL("/", req.url));
+//       }
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized: (params) => {
+//         let { token } = params;
+//         return !!token;
+//       },
+//     },
+//     pages: {
+//       signIn: "/login",
+//     },
+//   }
+// );
 
 export const config = {
-  matcher: ["/settings", "/skills", "/dashboard"],
+  matcher: [
+    "/login",
+    "/register",
+    "/verify",
+    "/settings",
+    "/skills",
+    "/dashboard",
+  ],
 };
