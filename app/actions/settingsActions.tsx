85503e00da315934ed446a46f8488513ba5d70cf
marsmallowz <alsandymaulana15@email.com>
2023-07-18 00:09:38 +0700

    add update profile

---
+++ app/actions/settingsActions.tsx
@@ -0,0 +1,108 @@
+"use server";
+
+import path from "path";
+import fs from "fs/promises";
+import { randomUUID } from "crypto";
+import os from "os";
+import { v2 as cloudinary } from "cloudinary";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/dist/client/components/headers";
+import { decode } from "next-auth/jwt";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+// jila type formData didefinisikan FormData malah menyebabkan error
+async function savePhotoToLocal(formData: any) {
+  const profileImage = formData.get("profileImage");
+  return await profileImage.arrayBuffer().then((data: any) => {
+    const buffer = Buffer.from(data);
+    const name = randomUUID();
+    const ext = profileImage.type.split("/")[1];
+    const tempDir = os.tmpdir();
+    const uploadDir = path.join(tempDir, `/${name}.${ext}`);
+    fs.writeFile(uploadDir, buffer);
+    return { filepath: uploadDir, filename: profileImage.name };
+  });
+
+  // not work in vercel
+  // const uploadDir = path.join(process.cwd(), "public", `/${name}.${ext}`);
+  // fs.writeFile(uploadDir, buffer);
+}
+
+export async function uploadPhoto(formData: FormData) {
+  try {
+    const sessionToken = cookies().get(
+      process.env.SESSION_TOKEN_NAME as string
+    );
+
+    const decoded = await decode({
+      token: sessionToken?.value,
+      secret: process.env.NEXTAUTH_SECRET as string,
+    });
+
+    if (!decoded?.sub) {
+      throw new Error("Invalid certification");
+    }
+
+    const user = await prisma?.user.findUnique({
+      where: {
+        id: decoded?.sub,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const username = formData.get("username");
+    const profileImage = formData.get("profileImage");
+
+    if (username !== null && (username as string) !== user.username) {
+      await prisma?.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          username: username as string,
+        },
+      });
+    }
+
+    if (profileImage !== null) {
+      const newFile = await savePhotoToLocal(formData);
+
+      if (!newFile) {
+        throw new Error("Image not found");
+      }
+
+      const res = await cloudinary.uploader.upload(newFile.filepath, {
+        folder: "nextjs_upload",
+      });
+
+      await prisma?.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          profileImageId: res.public_id,
+          profileImage: res.secure_url,
+        },
+      });
+
+      if (user.profileImageId !== null) {
+        await cloudinary.uploader.destroy(user.profileImageId);
+      }
+
+      fs.unlink(newFile.filepath);
+    }
+
+    return { status: "success" };
+  } catch (error) {
+    console.log(error);
+    return { status: "failed" };
+  }
+}
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main className="p-2">Settings</main>;
+import getCurrentUser from "../actions/getCurrentUser";
+import FormUpdateProfile from "../../components/FormUpdateProfile";
+
+export default async function Page() {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <div>
+      <FormUpdateProfile currentUser={currentUser} />
+    </div>
+  );
 }
