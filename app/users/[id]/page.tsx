c5428240f284c0a803f40f66c0617bfbcc8db2dc
marsmallowz <alsandymaulana15@email.com>
2023-07-18 00:10:08 +0700

    update page user show profileImage

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getUser } from "../../actions/userActions";
 import getCurrentUser from "../../actions/getCurrentUser";
 import CreateExchangeBtn from "../../../components/CreateExchangeBtn";
 import FormCreateExchangeModal from "../../../components/FormCreateExchangeModal";
+import Image from "next/image";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { user, userRating } = await getUser(params.id);
@@ -10,13 +11,20 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <main className="flex flex-col p-2 gap-3 items-start">
+      <Image
+        src={
+          user?.profileImage !== null
+            ? user?.profileImage!
+            : "/profile-image-default.png"
+        }
+        alt="profile image"
+        width={250}
+        height={250}
+        className="rounded-full"
+      />
       <div className="flex flex-col gap-0.5">
         <h1 className="font-medium text-lg">Username: </h1>
         <div>{user?.username}</div>
-      </div>
-      <div className="flex flex-col gap-0.5">
-        <h1 className="font-medium text-lg">Rate: </h1>
-        <div>{userRating}/5</div>
       </div>
       <div className="flex flex-col gap-0.5">
         <h1 className="font-medium text-lg">Offer Skills:</h1>
@@ -49,6 +57,12 @@ export default async function Page({ params }: { params: { id: string } }) {
             <div>Request skills not found</div>
           )}
         </ul>
+      </div>
+      <div className="flex flex-col gap-0.5">
+        <h1 className="font-medium text-lg">Rate: </h1>
+        <div>
+          {user?.reviewed.length ? `${userRating}/5` : "Belum dapat ditentukan"}
+        </div>
       </div>
       <div className="flex flex-col gap-0.5 w-full">
         <h1 className="font-medium text-lg">Reviews: </h1>
