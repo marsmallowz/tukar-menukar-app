5747a35cd8c3782929cf4afff8f0470f50301f08
marsmallowz <alsandymaulana15@email.com>
2023-07-18 21:45:00 +0700

    add more info is user succes set password

--- components/FormSetPassword.tsx
+++ components/FormSetPassword.tsx
@@ -88,6 +88,7 @@ export default function FormSetPassword({
           ? "Verifikasi Gagal"
           : ""}
       </div>
+      <div>{status === "success" ? "Silahkan Masuk" : ""}</div>
     </div>
   );
 }
