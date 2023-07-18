3176fceafedfc50719c2f3a83ca22a4ba65a935e
marsmallowz <alsandymaulana15@email.com>
2023-07-18 21:42:36 +0700

    fix middleware cannot open register page and other

--- middleware.tsx
+++ middleware.tsx
@@ -2,61 +2,65 @@ import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(
-  req: NextRequest,
-  event: NextFetchEvent
-) {
-  const token = await getToken({ req });
-  const isAuthenticated = !!token;
+// export default async function middleware(
+//   req: NextRequest,
+//   event: NextFetchEvent
+// ) {
+//   const token = await getToken({ req });
+//   const isAuthenticated = !!token;
 
-  if (
-    req.nextUrl.pathname.startsWith("/login") ||
-    req.nextUrl.pathname.startsWith("/register") ||
-    req.nextUrl.pathname.startsWith("/verify")
-  ) {
-    if (isAuthenticated) {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
-  }
-  const authMiddleware = await withAuth({
-    pages: {
-      signIn: `/login`,
-    },
-  });
-
-  // @ts-expect-error
-  return authMiddleware(req, event);
-}
-
-// tidak bisa digunakan karena terjadi bug jika menggunakan matcher "/login"
-// export default withAuth(
-//   async function middleware(req) {
-//     const token = await getToken({ req });
-//     console.log(token);
-//     const isAuthenticated = !!token;
-
-//     if (
-//       req.nextUrl.pathname.startsWith("/login") ||
-//       req.nextUrl.pathname.startsWith("/register") ||
-//       req.nextUrl.pathname.startsWith("/verify")
-//     ) {
-//       if (isAuthenticated) {
-//         return NextResponse.redirect(new URL("/", req.url));
-//       }
+//   if (
+//     req.nextUrl.pathname.startsWith("/login")
+//     // ||
+//     // req.nextUrl.pathname.startsWith("/register") ||
+//     // req.nextUrl.pathname.startsWith("/verify")
+//   ) {
+//     console.log(req.url);
+//     if (isAuthenticated) {
+//       return NextResponse.redirect(new URL("/dashboard", req.url));
 //     }
-//   },
-//   {
-//     callbacks: {
-//       authorized: (params) => {
-//         let { token } = params;
-//         return !!token;
-//       },
-//     },
-//     pages: {
-//       signIn: "/login",
-//     },
+//     //  else {
+//     //   if (req.nextUrl.pathname.startsWith("/login")) {
+//     //     return NextResponse.redirect(new URL("/login", req.url));
+//     //   } else if (req.nextUrl.pathname.startsWith("/register")) {
+//     //     return NextResponse.redirect(new URL("/register", req.url));
+//     //   } else if (req.nextUrl.pathname.startsWith("/verify")) {
+//     //     return NextResponse.redirect(new URL("/verify", req.url));
+//     //   }
+//     // }
 //   }
-// );
+//   const authMiddleware = await withAuth({
+//     pages: {
+//       signIn: `/login`,
+//     },
+//   });
+
+//   // @ts-expect-error
+//   return authMiddleware(req, event);
+// }
+
+export default withAuth(async function middleware(req) {}, {
+  callbacks: {
+    authorized: (params) => {
+      let { token, req } = params;
+      // console.log(req.nextUrl.pathname.startsWith("/register"));
+      // console.log(!!token);
+
+      if (!!token) {
+        if (
+          req.nextUrl.pathname.startsWith("/register") ||
+          req.nextUrl.pathname.startsWith("/login") ||
+          req.nextUrl.pathname.startsWith("/verify")
+        ) {
+          return false;
+        }
+      } else {
+        return true;
+      }
+      return !!token;
+    },
+  },
+});
 
 export const config = {
   matcher: [
